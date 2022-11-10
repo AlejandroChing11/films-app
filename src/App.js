e942158a9c74a6cd72235f9c760e09c111d70c3f
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-10 16:57:31 -0500

    Adding html to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Film from './Film';
+import PageWrapper from './PageWrapper';
 
 function App() {
+  let peliculas = [
+    {
+      title: "Oblivion (2012)",
+      calification: "8.1",
+      director: "Joss Whedon",
+      actors: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+      date: "1 may 2015",
+      duration: "2h 21mins",
+      img: "images/uploads/mv1.jpg"
+    },
+    {
+      title:"into the wild (2014)",
+      calification: "7.8", 
+      director:"Anthony Russo, Joe Russo", 
+      actors:"Chris Evans, Samuel L. Jackson, Scarlett Johansson",
+      date:"1 May 2015", 
+      duration:"2h 21mins", 
+      img:"images/uploads/mv2.jpg"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      <Film title="Oblivion (2012)" calification="8.1" director="Joss Whedon" actors="Robert Downey Jr., Chris Evans, Chris Hemsworth" date="1 may 2015" duration="2h 21mins"
+        img="images/uploads/mv1.jpg">
+        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+      </Film>
+
+      
+    </PageWrapper>
+
   );
 }
 
---
+++ src/Film.js
@@ -0,0 +1,25 @@
+export default function Film(props) {
+    return (
+        <div>
+            <div className="movie-item-style-2">
+                <img src={props.img} alt="" />
+                <div className="mv-item-infor">
+                    <h6>
+                        <a href="moviesingle.html">
+                            {props.title}
+                        </a>
+                    </h6>
+                    <p className="rate">
+                        <i className="ion-android-star"></i>
+                        <span>{props.calification}</span> /10</p>
+                    <p className="describe">{props.children}</p>
+                    <p className="run-time"> Run Time: {props.duration}    .     <span>MMPA: PG-13 </span>    .     <span>{props.date}</span></p>
+                    <p>Director: <a href="#">{props.director}</a></p>
+                    <p>Stars: <a href="#"> {props.actors} </a></p>
+                </div>
+            </div>
+        </div>
+
+        
+    );
+}
